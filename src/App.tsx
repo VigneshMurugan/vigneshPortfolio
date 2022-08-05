a65539bc9a457ed226df0e8250d101e0cf1cd5ff
Vignesh Murugan <vignesh328@gmail.com>
2022-08-05 23:23:52 +0530

    Home screen modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/nav";
+import developer from "./developerAvatar.png"
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar 
-          links = {
-            ["home", "about", "services", "portfolio"]
-          }
-        />
+        <NavBar links={["home", "about", "services", "portfolio"]} />
+        <div id="heroBanner">
+          <div id="leftHeroWrapper">
+            <p>Ahoy ! I'm Vignesh Murugan</p>
+            <p>WEB DEVELOPER | REACT JS | NODE JS</p>
+            <p>I am an experienced Web Developer proficient in </p>
+            <p>React JS, Node JS based out of Bengaluru, India</p>
+          </div>
+          <div id="rightHeroWrapper">
+            <img id="avatarImage" src={developer} />
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
