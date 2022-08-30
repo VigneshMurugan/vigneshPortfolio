794dbb7734e9a4947fab99cec0b5f2c1ffa5a991
Vignesh Murugan <vignesh328@gmail.com>
2022-08-30 21:42:50 +0530

    Clean Slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/nav";
-import developer from "./developerAvatar.png"
-
-
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar links={["home", "about", "services", "portfolio", "testimonials", "contact"]} />
-        <div id="heroBanner">
-          <img id="avatarImage" src={developer} />
-        </div>
-      </BrowserRouter>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: [
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    ],
+    plugins:  [require('flowbite/plugin')],
+  };
